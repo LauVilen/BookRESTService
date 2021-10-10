330f97b0283a8fad0308673ff6c29fceaf01478b
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2021-10-10 18:04:07 +0200

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookRESTService.Managers;
+using BookLibrary;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookRESTService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookManager _manager = new BookManager();
+        
+
+        // GET: api/<BookController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public Book Get(string isbn13)
+        {
+            return _manager.GetById(isbn13);
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public Book Post([FromBody] Book book)
+        {
+            return _manager.Add(book);
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public Book Put(string isbn, [FromBody] Book updateBook)
+        {
+            return _manager.Update(isbn, updateBook);
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public Book Delete(string isbn)
+        {
+            return _manager.Delete(isbn);
+        }
+    }
+}
---
+++ Managers/BookManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using BookLibrary;
+
+namespace BookRESTService.Managers
+{
+    public class BookManager
+    {
+        private static readonly List<Book> _books = new List<Book>
+        {
+            new Book(){ISBN13 = "9780609806951", Title = "Who Cooked the Last Supper?", Author = "Rosalind Miles", NumberOfPages = 352},
+            new Book(){ISBN13 = "9781911344056", Title = "South of Forgiveness", Author = "Thordis Elva", NumberOfPages = 320},
+            new Book(){ISBN13 = "9788702055023", Title = "Paula", Author = "Isabel Allende", NumberOfPages = 337}
+        };
+        public List<Book> GetAll()
+        {
+            return new List<Book>(_books);
+        }
+
+        public Book GetById(string isbn)
+        {
+            return _books.Find(book => book.ISBN13 == isbn);
+        }
+
+        public Book Add(Book newBook)
+        {
+            if (!_books.Exists(book => newBook.ISBN13 == book.ISBN13))
+            {
+                _books.Add(newBook);
+                return newBook;
+            }
+            else
+            {
+                throw new DuplicateNameException(
+                    "This ISBN number already exists in the database. Please try another ISBN number");
+            }
+        }
+
+        public Book Delete(string isbn)
+        {
+            Book bookToDelete = _books.Find(book => book.ISBN13 == isbn);
+            if (bookToDelete == null) return null;
+            _books.Remove(bookToDelete);
+            return bookToDelete;
+        }
+
+        public Book Update(string isbn, Book updateBook)
+        {
+            Book bookToUpdate = _books.Find(book => book.ISBN13 == isbn);
+            if (bookToUpdate == null) return null;
+            bookToUpdate.Title = updateBook.Title;
+            bookToUpdate.Author = updateBook.Author;
+            bookToUpdate.NumberOfPages = updateBook.NumberOfPages;
+            bookToUpdate.ISBN13 = bookToUpdate.ISBN13;
+            return bookToUpdate;
+        }
+    }
+}
